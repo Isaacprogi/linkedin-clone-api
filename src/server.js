68e0c853acf311fd008ab12b49f4f6e377a2a9a3
isaac <isaaconyes80@gmail.com>
2023-05-22 00:26:15 +0100

    remake

--- src/server.js
+++ src/server.js
@@ -27,7 +27,7 @@ const User = require('./models/User')
 
 //middle wares
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://itslinkedinclone.onrender.com',
   preflightContinue: false,
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true
@@ -60,7 +60,7 @@ const server = http.createServer(app)
 
 const io = new Server(server,{
     cors: {
-      origin: 'http://localhost:3000'
+      origin: 'https://itslinkedinclone.onrender.com'
     },
   })
 
